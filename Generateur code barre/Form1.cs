706fb7b370d2b7f5c2883a567623f9cfbd474a91
Elmahdi <Elmahdi@ELMAHDI>
2022-10-30 23:34:40 +0100

    Ajoutez des fichiers projet.

---
+++ Generateur code barre/Form1.cs
@@ -0,0 +1,31 @@
+﻿using IronOcr;
+using System;
+using System.Windows.Forms;
+
+
+namespace Generateur_code_barre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ocr = new IronTesseract();
+            using(var input = new OcrInput(@"C:\Users\Elmahdi\Desktop\R(1).jfif"))
+            {
+                var result = ocr.Read(input);
+                result.SaveAsTextFile(@"C:\Users\Elmahdi\Desktop\x.txt");
+
+            }
+        }
+    }
+}
