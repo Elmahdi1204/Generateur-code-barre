4b3fd909d3fedeae2a456b24f7465ebedfac1033
mehdi benzaoui <mahdi-benzaoui@live.fr>
2022-11-18 01:13:55 +0100

    Elmahdi

--- Generateur code barre/Form1.Designer.cs
+++ Generateur code barre/Form1.Designer.cs
@@ -47,12 +47,13 @@ namespace Generateur_code_barre
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(107, 84);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(435, 54);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
